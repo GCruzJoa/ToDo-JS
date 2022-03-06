ecee754c0dbbd53f7581e08b14ee739b91a7b444
Gabriel <gabriel.cruzjoa@gmail.com>
2022-03-06 18:30:58 -0400

    Correciones

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './styles.css';
 import { Todo, TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
-// import { Todo } from './classes/todo.class.js';
-// import { TodoList } from './classes/todo-list.class.js';
 
 export const todoList = new TodoList();
 
